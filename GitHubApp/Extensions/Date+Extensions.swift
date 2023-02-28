300e5ce01c1d404f7dd4dad1e355788b805728f7
Frédéric Helfer <fre.helfer@gmail.com>
2023-02-28 14:21:39 -0300

    implement: date format method

--- GitHubApp/Extensions/Date+Extensions.swift
+++ GitHubApp/Extensions/Date+Extensions.swift
@@ -4,7 +4,16 @@ extension Date {
     
     static func format(dateString: String) -> String? {
         
-        // TODO
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        
+        let date = dateFormatter.date(from: dateString)
+        
+        if let date {
+            dateFormatter.dateStyle = .short
+            return dateFormatter.string(from: date)
+        }
+        
         return nil
     }
     
